f30321ea45b0c3b4314297ce1aa574c029046248
Dunn, Simeon <simedunn01@gmail.com>
2026-06-09 20:20:46 -0400

    init sveltekit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import favicon from "$lib/assets/favicon.svg";
+  import "../app.css";
+  import "@fontsource-variable/libre-franklin/wght.css";
+  import "@fontsource/barlow-condensed/400.css";
+  import "@fontsource/barlow-condensed/800.css";
+
+  let { children } = $props();
+</script>
+
+<svelte:head> <link rel="icon" href={favicon}> </svelte:head>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="text-3xl text-teal-500">Woloroofing!</h1>
