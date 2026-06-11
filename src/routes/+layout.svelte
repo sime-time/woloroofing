07bbf297c71f326d6e02924f2a57c12c46988de7
Dunn, Simeon <simedunn01@gmail.com>
2026-06-11 19:00:40 -0400

    og-image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,6 +17,19 @@
   <link rel="icon" href={favicon}>
   <title>{title}</title>
   <meta name="description" content={description}>
+
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://woloroofing.com/">
+  <meta property="og:title" content={title}>
+  <meta property="og:description" content={description}>
+  <meta property="og:image" content="https://woloroofing.com/og-image.jpg">
+  <meta property="og:image:width" content="1200">
+  <meta property="og:image:height" content="630">
+
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:title" content={title}>
+  <meta name="twitter:description" content={description}>
+  <meta name="twitter:image" content="https://woloroofing.com/og-image.jpg">
 </svelte:head>
 
 {@render children()}
