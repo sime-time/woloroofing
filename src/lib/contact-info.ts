71009ba5ad4ce7cf279336179b745d46d55c3a63
Dunn, Simeon <simedunn01@gmail.com>
2026-07-15 18:16:32 -0400

    fix phone number typo

--- src/lib/contact-info.ts
+++ src/lib/contact-info.ts
@@ -1,6 +1,6 @@
 export const WOLOPHONE = "847-682-5236";
 export const WOLOEMAIL = "info@woloroofing.com";
-export const WOLOPHONE_HREF = "tel:+18746825236";
+export const WOLOPHONE_HREF = "tel:+18476825236";
 export const WOLOEMAIL_HREF = `mailto:${WOLOEMAIL}`;
 export const SMS_CONSENT_TEXT =
   "I agree to receive text messages from WOLO Roofing about my inspection request. Msg & data rates may apply.";
